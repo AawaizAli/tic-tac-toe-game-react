6f97673a11632dbc7cb98a63421a45d3802e51fa
Aawaiz Ali <114678240+AawaizAli@users.noreply.github.com>
2024-06-06 18:28:12 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Game from '../../components/Game.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Game />
+    </>);
+}
+
+export default App
